2f2efb1e4e58d2a8d4c23779cd7cc3bdb7cd1e02
Tim Kaechle <timothy.kaechle@me.com>
2022-10-30 21:27:44 +0000

    finalized colors for dark mode

--- Themes/test.js
+++ Themes/test.js
@@ -11,12 +11,36 @@ node.app("callback", function (callbackData) {
 
 // es6
 
-node.app("callback", (callbackdata) => callbackData.filter())
+node.app("callback", (callbackData) => callbackData.filter())
 
 const constantVar = document.querySelector("#header")
 var nums = 10 + 10 === 20
 let str = "string"
 let bool = true || (false && 10 >= 10)
+
+if (constantVar == str) {
+  obj.pop(nums)
+  bool = !bool
+  let newValue = compose(unfold(obj))
+  pluckDeep(newValue)
+  return newValue
+}
+
+const pluckDeep = (key) => (obj) =>
+  key.split(".").reduce((accum, key) => accum[key], obj)
+
+const compose =
+  (...fns) =>
+  (res) =>
+    fns.reduce((accum, next) => next(accum), res)
+
+const unfold = (f, seed) => {
+  const go = (f, seed, acc) => {
+    const res = f(seed)
+    return res ? go(f, res[1], acc.concat([res[0]])) : acc
+  }
+  return go(f, seed, [])
+}
 
 const obj = {
   key: [
